955f991bc17273b9a9ce91f350156da1630e674d
Martin Lemesle <lemesle.martin@hotmail.fr>
2023-07-15 17:00:28 +0200

    Add full impl for Node4 with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::fmt;
+use std::{
+    fmt,
+    mem::{swap, take},
+};
 
 /*
 Additionally, at the front of each inner node, a header of
@@ -78,16 +81,79 @@ impl Node {
                 // (InnerNode::Node4(value), None)
             }
 
-            InnerNode::Node4(node) => {
-                // match input.strip_prefix(&self.path) {
-                //     Some([]) => ,
-                //     None => todo!(),
-                // }
-
-                let (node, old_value) = node.insert(input, value);
-                self.inner = node;
-                old_value
-            }
+            InnerNode::Node4(ref mut node) => match input.strip_prefix(self.path.as_slice()) {
+                Some([]) => {
+                    if node.keys[0] == EOption::End {
+                        let mut value_node = node.values[0].take().unwrap();
+                        let value_node_v = value_node.inner.unwrap_leaf();
+                        let old_value = *value_node_v;
+                        *value_node_v = value;
+                        node.values[0] = Some(value_node);
+                        Some(old_value)
+                    } else {
+                        if self.nb_childrens == 4 {
+                            let mut new_node = Node16::from(take(node));
+                            new_node.keys.rotate_right(1);
+                            new_node.keys[0] = EOption::End;
+                            new_node.values.rotate_right(1);
+                            new_node.values[0] =
+                                Some(Box::new(Node::default().insert(&[], value).0));
+                            self.inner = InnerNode::Node16(new_node);
+                        } else {
+                            node.keys.rotate_right(1);
+                            node.keys[0] = EOption::End;
+                            node.values.rotate_right(1);
+                            node.values[0] = Some(Box::new(Node::default().insert(&[], value).0));
+                        }
+                        None
+                    }
+                }
+                Some(s) => {
+                    if let Some(pos) = node
+                        .keys
+                        .iter()
+                        .position(|k| matches!(k, EOption::Some(b) if *b == s[0]))
+                    {
+                        let (new_node, old_value) =
+                            node.values[pos].take().unwrap().insert(s, value);
+                        node.values[pos] = Some(Box::new(new_node));
+                        old_value
+                    } else {
+                        if self.nb_childrens == 4 {
+                            let mut new_node = Node16::from(take(node));
+                            let pos = new_node
+                                .keys
+                                .iter()
+                                .position(|k| {
+                                    *k == EOption::None
+                                        || matches!(k, EOption::Some(b) if *b > s[0])
+                                })
+                                .unwrap();
+                            new_node.keys[pos..].rotate_right(1);
+                            new_node.keys[pos] = EOption::Some(s[0]);
+                            new_node.values[pos..].rotate_right(1);
+                            new_node.values[pos] =
+                                Some(Box::new(Node::default().insert(s, value).0));
+                            self.inner = InnerNode::Node16(new_node);
+                        } else {
+                            let pos = node
+                                .keys
+                                .iter()
+                                .position(|k| {
+                                    *k == EOption::None
+                                        || matches!(k, EOption::Some(b) if *b > s[0])
+                                })
+                                .unwrap();
+                            node.keys[pos..].rotate_right(1);
+                            node.keys[pos] = EOption::Some(s[0]);
+                            node.values[pos..].rotate_right(1);
+                            node.values[pos] = Some(Box::new(Node::default().insert(s, value).0));
+                        }
+                        None
+                    }
+                }
+                None => None,
+            },
             // InnerNode::Node16(node) => node.insert(input),
             // InnerNode::Node48(node) => node.insert(input),
             // InnerNode::Node256(node) => node.insert(input),
@@ -111,6 +177,17 @@ pub enum InnerNode {
     Node16(Node16),
     Node48(Node48),
     Node256(Node256),
+}
+
+impl InnerNode {
+    #[track_caller]
+    pub fn unwrap_leaf(&mut self) -> &mut u64 {
+        if let Self::SingleValueLeaf(ref mut value) = self {
+            value
+        } else {
+            panic!("Unwrapped a non-leaf in `unwrap_leaf`")
+        }
+    }
 }
 
 #[derive(Default, Debug, PartialEq)]
@@ -139,31 +216,6 @@ pub struct Node4 {
     values: [Option<Box<Node>>; 4],
 }
 
-impl Node4 {
-    fn insert(mut self, input: &[u8], value: u64) -> (InnerNode, Option<u64>) {
-        let byte = input[0];
-        let pos = self.keys.iter().position(|opt| *opt == EOption::Some(byte));
-
-        if let Some(pos) = pos {
-            // safe because we found the position above
-            /*
-            let inserted = self.values[pos]
-                .as_mut()
-                .unwrap()
-                .insert(&input[1..], value);
-            */
-            todo!()
-            // (InnerNode::Node4(self), inserted)
-        } else if let Some(pos) = self.keys.iter().position(EOption::is_none) {
-            // create a leaf
-            todo!()
-        } else {
-            // move to a Node16 and add a new leaf
-            todo!()
-        }
-    }
-}
-
 impl fmt::Debug for Node4 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let keys = self
@@ -171,8 +223,8 @@ impl fmt::Debug for Node4 {
             .iter()
             .map(|key| match key {
                 EOption::Some(k) => format!("`{}`", *k as char),
-                EOption::None => format!("___"),
-                EOption::End => format!("END"),
+                EOption::None => "___".to_string(),
+                EOption::End => "END".to_string(),
             })
             .collect::<Vec<String>>();
         f.debug_struct("Node")
@@ -191,13 +243,22 @@ parallel comparisons using SIMD instructions.
 */
 #[derive(Debug)]
 pub struct Node16 {
-    keys: [Option<u8>; 16],
+    keys: [EOption<u8>; 16],
     values: [Option<Box<Node>>; 16],
 }
 
-impl Node16 {
-    fn insert(&mut self, input: &[u8]) -> Option<u64> {
-        todo!()
+impl From<Node4> for Node16 {
+    fn from(mut value: Node4) -> Self {
+        let mut keys: [EOption<u8>; 16] = Default::default();
+        keys.iter_mut()
+            .zip(value.keys.iter_mut())
+            .for_each(|(l, r)| swap(l, r));
+        let mut values: [Option<Box<Node>>; 16] = Default::default();
+        values
+            .iter_mut()
+            .zip(value.values.iter_mut())
+            .for_each(|(l, r)| swap(l, r));
+        Self { keys, values }
     }
 }
 
@@ -218,12 +279,6 @@ pub struct Node48 {
     values: [Option<Box<Node>>; 48],
 }
 
-impl Node48 {
-    fn insert(&mut self, input: &[u8]) -> Option<u64> {
-        todo!()
-    }
-}
-
 /*
 Node256: The largest node type is simply an array of 256
 pointers and is used for storing between 49 and 256 entries.
@@ -236,12 +291,6 @@ only pointers need to be stored.
 #[derive(Debug)]
 pub struct Node256 {
     values: [Option<Box<Node>>; 256],
-}
-
-impl Node256 {
-    fn insert(&mut self, input: &[u8]) -> Option<u64> {
-        todo!()
-    }
 }
 
 #[derive(Default, Debug)]
@@ -385,6 +434,300 @@ mod test {
                     Node {
                         keys: "[\"END\", \"`o`\", \"___\", \"___\"]",
                         values: "[Some(Node { nb_childrens: 1, path: \"`` ([])\", inner: SingleValueLeaf(43) }), Some(Node { nb_childrens: 1, path: \"`o` ([111])\", inner: SingleValueLeaf(42) }), None, None]",
+                    },
+                ),
+            },
+        }
+        "###);
+    }
+
+    #[test]
+    fn reinsert_a_value_in_node4() {
+        let mut art = Art::new();
+        art.insert(b"hello", 42);
+        art.insert(b"hell", 43);
+        let ret = art.insert(b"hell", 44);
+        insta::assert_debug_snapshot!(ret, @r###"
+        Some(
+            43,
+        )
+        "###);
+
+        insta::assert_debug_snapshot!(art, @r###"
+        Art {
+            root: Node {
+                nb_childrens: 2,
+                path: "`hell` ([104, 101, 108, 108])",
+                inner: Node4(
+                    Node {
+                        keys: "[\"END\", \"`o`\", \"___\", \"___\"]",
+                        values: "[Some(Node { nb_childrens: 1, path: \"`` ([])\", inner: SingleValueLeaf(44) }), Some(Node { nb_childrens: 1, path: \"`o` ([111])\", inner: SingleValueLeaf(42) }), None, None]",
+                    },
+                ),
+            },
+        }
+        "###);
+    }
+
+    #[test]
+    fn insert_a_prefix_value_in_node4() {
+        let mut art = Art::new();
+        art.insert(b"hello", 42);
+        art.insert(b"hella", 43);
+        let ret = art.insert(b"hell", 44);
+        insta::assert_debug_snapshot!(ret, @"None");
+
+        insta::assert_debug_snapshot!(art, @r###"
+        Art {
+            root: Node {
+                nb_childrens: 3,
+                path: "`hell` ([104, 101, 108, 108])",
+                inner: Node4(
+                    Node {
+                        keys: "[\"END\", \"`a`\", \"`o`\", \"___\"]",
+                        values: "[Some(Node { nb_childrens: 1, path: \"`` ([])\", inner: SingleValueLeaf(44) }), Some(Node { nb_childrens: 1, path: \"`a` ([97])\", inner: SingleValueLeaf(43) }), Some(Node { nb_childrens: 1, path: \"`o` ([111])\", inner: SingleValueLeaf(42) }), None]",
+                    },
+                ),
+            },
+        }
+        "###);
+    }
+
+    #[test]
+    fn insert_a_prefix_value_in_a_full_node4() {
+        let mut art = Art::new();
+        art.insert(b"hello", 42);
+        art.insert(b"hella", 43);
+        art.insert(b"helli", 44);
+        art.insert(b"hellu", 45);
+        let ret = art.insert(b"hell", 46);
+        insta::assert_debug_snapshot!(ret, @"None");
+
+        insta::assert_debug_snapshot!(art, @r###"
+        Art {
+            root: Node {
+                nb_childrens: 5,
+                path: "`hell` ([104, 101, 108, 108])",
+                inner: Node16(
+                    Node16 {
+                        keys: [
+                            End,
+                            Some(
+                                97,
+                            ),
+                            Some(
+                                105,
+                            ),
+                            Some(
+                                111,
+                            ),
+                            Some(
+                                117,
+                            ),
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                        ],
+                        values: [
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`` ([])",
+                                    inner: SingleValueLeaf(
+                                        46,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`a` ([97])",
+                                    inner: SingleValueLeaf(
+                                        43,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`i` ([105])",
+                                    inner: SingleValueLeaf(
+                                        44,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`o` ([111])",
+                                    inner: SingleValueLeaf(
+                                        42,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`u` ([117])",
+                                    inner: SingleValueLeaf(
+                                        45,
+                                    ),
+                                },
+                            ),
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                        ],
+                    },
+                ),
+            },
+        }
+        "###);
+    }
+
+    #[test]
+    fn insert_a_value_in_a_full_node4() {
+        let mut art = Art::new();
+        art.insert(b"hello", 42);
+        art.insert(b"hella", 43);
+        art.insert(b"helli", 44);
+        art.insert(b"hellu", 45);
+        let ret = art.insert(b"hellyolo", 46);
+        insta::assert_debug_snapshot!(ret, @"None");
+
+        insta::assert_debug_snapshot!(art, @r###"
+        Art {
+            root: Node {
+                nb_childrens: 5,
+                path: "`hell` ([104, 101, 108, 108])",
+                inner: Node16(
+                    Node16 {
+                        keys: [
+                            Some(
+                                97,
+                            ),
+                            Some(
+                                105,
+                            ),
+                            Some(
+                                111,
+                            ),
+                            Some(
+                                117,
+                            ),
+                            Some(
+                                121,
+                            ),
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                        ],
+                        values: [
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`a` ([97])",
+                                    inner: SingleValueLeaf(
+                                        43,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`i` ([105])",
+                                    inner: SingleValueLeaf(
+                                        44,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`o` ([111])",
+                                    inner: SingleValueLeaf(
+                                        42,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`u` ([117])",
+                                    inner: SingleValueLeaf(
+                                        45,
+                                    ),
+                                },
+                            ),
+                            Some(
+                                Node {
+                                    nb_childrens: 1,
+                                    path: "`yolo` ([121, 111, 108, 111])",
+                                    inner: SingleValueLeaf(
+                                        46,
+                                    ),
+                                },
+                            ),
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                            None,
+                        ],
+                    },
+                ),
+            },
+        }
+        "###);
+    }
+
+    #[test]
+    fn insert_several_values() {
+        let mut art = Art::new();
+        art.insert(b"hello", 42);
+        art.insert(b"hella", 43);
+        let ret = art.insert(b"helli", 44);
+        insta::assert_debug_snapshot!(ret, @"None");
+
+        insta::assert_debug_snapshot!(art, @r###"
+        Art {
+            root: Node {
+                nb_childrens: 3,
+                path: "`hell` ([104, 101, 108, 108])",
+                inner: Node4(
+                    Node {
+                        keys: "[\"`a`\", \"`i`\", \"`o`\", \"___\"]",
+                        values: "[Some(Node { nb_childrens: 1, path: \"`a` ([97])\", inner: SingleValueLeaf(43) }), Some(Node { nb_childrens: 1, path: \"`i` ([105])\", inner: SingleValueLeaf(44) }), Some(Node { nb_childrens: 1, path: \"`o` ([111])\", inner: SingleValueLeaf(42) }), None]",
                     },
                 ),
             },
